f769dea16071927744df9075ca12caf99a1b80a4
Derek Ellis <derekellis170@gmail.com>
2023-02-04 20:34:03 -0500

    Update sample with map controls
    Add sample data

--- sample/src/jsMain/kotlin/Main.kt
+++ sample/src/jsMain/kotlin/Main.kt
@@ -1,39 +1,45 @@
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import ca.derekellis.mapbox.MapboxMap
 import ca.derekellis.mapbox.rememberMapboxState
 import com.sample.BuildKonfig
+import geojson.GeoJsonObject
+import org.jetbrains.compose.web.attributes.InputType
 import org.jetbrains.compose.web.css.Style
-import org.jetbrains.compose.web.css.StyleSheet
 import org.jetbrains.compose.web.css.height
-import org.jetbrains.compose.web.css.margin
-import org.jetbrains.compose.web.css.percent
-import org.jetbrains.compose.web.css.px
 import org.jetbrains.compose.web.css.vh
 import org.jetbrains.compose.web.css.vw
 import org.jetbrains.compose.web.css.width
+import org.jetbrains.compose.web.dom.Button
+import org.jetbrains.compose.web.dom.Div
+import org.jetbrains.compose.web.dom.Input
+import org.jetbrains.compose.web.dom.Label
+import org.jetbrains.compose.web.dom.RangeInput
+import org.jetbrains.compose.web.dom.Text
 import org.jetbrains.compose.web.renderComposable
 
 external fun require(module: String): dynamic
 
-object MainStyleSheet : StyleSheet() {
-  init {
-    "html, body, #root" style {
-      width(100.percent)
-      height(100.percent)
-      margin(0.px)
-    }
-  }
-}
-
 fun main() {
   require("mapbox-gl/dist/mapbox-gl.css")
+  val data = require("./data.json")
 
   renderComposable(rootElementId = "root") {
     val mapState = rememberMapboxState()
 
+    var color by remember { mutableStateOf("#000000") }
+    var showLayer by remember { mutableStateOf(true) }
+    var circleSize by remember { mutableStateOf(5) }
+    var isDarkTheme by remember { mutableStateOf(false) }
+
+    if (isDarkTheme) Style(Dark) else Style(Light)
     Style(MainStyleSheet)
+
     MapboxMap(
       accessToken = BuildKonfig.MAPBOX_ACCESS_TOKEN,
-      style = "mapbox://styles/mapbox/light-v10",
+      style = if (isDarkTheme) "mapbox://styles/mapbox/dark-v10" else "mapbox://styles/mapbox/light-v10",
       state = mapState,
       containerAttrs = {
         style {
@@ -42,7 +48,46 @@ fun main() {
         }
       },
     ) {
-      // TODO: Add sources
+      GeoJsonSource("test", data = data.unsafeCast<GeoJsonObject>()) {
+        if (showLayer) {
+          CircleLayer("circles") {
+            circleColor(color)
+            circleRadius(circleSize)
+          }
+        }
+      }
+    }
+
+    // Map Controls
+    Div(attrs = { classes(MainStyleSheet.overlay) }) {
+      Div {
+        Label(forId = "color") { Text("Layer Color:") }
+        Input(InputType.Color, attrs = {
+          id("color")
+          onInput { color = it.value }
+        })
+      }
+      Div {
+        Label(forId = "layer-toggle") { Text("Toggle Layer:") }
+        Button(attrs = {
+          id("layer-toggle")
+          onClick { showLayer = !showLayer }
+        }) { Text(if (showLayer) "ON" else "OFF") }
+      }
+      Div {
+        Label(forId = "circle-size") { Text("Circle Size:") }
+        RangeInput(value = circleSize, min = 5, max = 30) {
+          id("circle-size")
+          onInput { circleSize = it.value?.toInt() ?: circleSize }
+        }
+      }
+      Div {
+        Label(forId = "theme-toggle") { Text("Toggle Theme:") }
+        Button(attrs = {
+          id("theme-toggle")
+          onClick { isDarkTheme = !isDarkTheme }
+        }) { Text(if (isDarkTheme) "Dark" else "Light") }
+      }
     }
   }
 }
---
+++ sample/src/jsMain/kotlin/Styles.kt
@@ -0,0 +1,64 @@
+import org.jetbrains.compose.web.css.CSSColorValue
+import org.jetbrains.compose.web.css.Color
+import org.jetbrains.compose.web.css.DisplayStyle
+import org.jetbrains.compose.web.css.FlexDirection
+import org.jetbrains.compose.web.css.Position
+import org.jetbrains.compose.web.css.StyleSheet
+import org.jetbrains.compose.web.css.color
+import org.jetbrains.compose.web.css.display
+import org.jetbrains.compose.web.css.flexDirection
+import org.jetbrains.compose.web.css.fontFamily
+import org.jetbrains.compose.web.css.gap
+import org.jetbrains.compose.web.css.height
+import org.jetbrains.compose.web.css.left
+import org.jetbrains.compose.web.css.margin
+import org.jetbrains.compose.web.css.padding
+import org.jetbrains.compose.web.css.percent
+import org.jetbrains.compose.web.css.position
+import org.jetbrains.compose.web.css.px
+import org.jetbrains.compose.web.css.top
+import org.jetbrains.compose.web.css.value
+import org.jetbrains.compose.web.css.variable
+import org.jetbrains.compose.web.css.width
+
+object MainStyle {
+  val textPrimary by variable<CSSColorValue>()
+}
+
+object Light : StyleSheet() {
+  init {
+    ":root" style {
+      MainStyle.textPrimary(Color("#000000"))
+    }
+  }
+}
+
+object Dark : StyleSheet() {
+  init {
+    ":root" style {
+      MainStyle.textPrimary(Color("#FFFFFF"))
+    }
+  }
+}
+
+object MainStyleSheet : StyleSheet() {
+  init {
+    "html, body, #root" style {
+      width(100.percent)
+      height(100.percent)
+      margin(0.px)
+      fontFamily("sans-serif")
+    }
+  }
+
+  val overlay by style {
+    position(Position.Fixed)
+    top(0.px)
+    left(0.px)
+    display(DisplayStyle.Flex)
+    flexDirection(FlexDirection.Column)
+    gap(8.px)
+    padding(8.px)
+    color(MainStyle.textPrimary.value())
+  }
+}
